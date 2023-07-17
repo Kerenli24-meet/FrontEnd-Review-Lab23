19599756b67c7428572ff8a63310607f53dcee65
Kerenli24-meet <s216767889@igalalon.ort.org.il>
2023-07-17 17:56:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ const btn = document.getElementById('btn');
 
   btn.addEventListener('click', function onClick(event) {
   
-  document.body.style.backgroundColor = 'salmon';
-
+  document.body.style.backgroundColor = 'lightyellow';
 
 });
+
+//function clickChangeButton() {
+  //var color = getRandomColor();
+  //document.getElemntById('back').style.backgroundColor;
+
+//}
+//function getRancomColor() {
+  //var letters = "0123456789ABCDF";
+  //var color = "#";
+  //for ( var x =0;x<6;x++){
+    //color += letters[Math.floor(Math.random()*16)];
+ // }
+  //return color;
+//}
